af250c951e5e72c4de0b7c170aa678d2ce0456c7
shunpii <shun.s0029@gmail.com>
2022-02-01 18:23:12 +0900

    [add]１つ目の選択肢に正誤判定を追加

--- javascript/index.js
+++ javascript/index.js
@@ -15,3 +15,10 @@ document.getElementsByTagName('button')[2].textContent = answers[2];
 document.getElementsByTagName('button')[3].textContent = answers[3];
 
 // ボタンをクリックしたら正誤判定
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    if (correct  === document.getElementsByTagName('button')[0].textContent) {
+        window.alert('正解!');
+    }else {
+        window.alert('不正解!');
+    }
+});
