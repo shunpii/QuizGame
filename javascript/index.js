02820cff56b1bbb876a4990adaa4b4fe8a0a22ba
shunpii <shun.s0029@gmail.com>
2022-02-01 18:48:23 +0900

    [add]リファクタリング

--- javascript/index.js
+++ javascript/index.js
@@ -7,15 +7,24 @@ const answers = [
 ];
 const correct = 'ニンテンドーDS';
 
-// 定数の文字列をHTMLに反映させる
-document.getElementById('js-question').textContent = question;
-
 const $button = document.getElementsByTagName('button')
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+// クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = question;
+    let buttonIndex = 0;
+    let buttonLength = $button.length;
+    while(buttonIndex < buttonLength){
+        // ここに命令を書く
+        $button[buttonIndex].textContent = answers[buttonIndex];
+        buttonIndex++;
+    }
+}
+setupQuiz();
+
+
+
+
 
 // ボタンをクリックしたら正誤判定
 $button[0].addEventListener('click', () => {
