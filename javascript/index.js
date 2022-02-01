2460f3fc3dce72e0da2fabc06694ad999176d5bf
shunpii <shun.s0029@gmail.com>
2022-02-01 19:05:37 +0900

    [add]リファクタリング

--- javascript/index.js
+++ javascript/index.js
@@ -8,12 +8,12 @@ const answers = [
 const correct = 'ニンテンドーDS';
 
 const $button = document.getElementsByTagName('button')
+const buttonLength = $button.length;
 
 // クイズの問題文、選択肢を定義
 const setupQuiz = () => {
     document.getElementById('js-question').textContent = question;
     let buttonIndex = 0;
-    let buttonLength = $button.length;
     while(buttonIndex < buttonLength){
         // ここに命令を書く
         $button[buttonIndex].textContent = answers[buttonIndex];
@@ -22,36 +22,20 @@ const setupQuiz = () => {
 }
 setupQuiz();
 
-
-
-
+const clickHndler = (e) => {
+    if (correct === e.target.textContent){
+        window.alert('正解!');
+    }else {
+        window.alert('不正解!');
+    }
+};
 
 // ボタンをクリックしたら正誤判定
-$button[0].addEventListener('click', () => {
-    if (correct === $button[0].textContent) {
-        window.alert('正解!');
-    }else {
-        window.alert('不正解!');
-    }
-});
-$button[1].addEventListener('click', () => {
-    if (correct === $button[1].textContent) {
-        window.alert('正解!');
-    }else {
-        window.alert('不正解!');
-    }
-});
-$button[2].addEventListener('click', () => {
-    if (correct === $button[2].textContent) {
-        window.alert('正解!');
-    }else {
-        window.alert('不正解!');
-    }
-});
-$button[3].addEventListener('click', () => {
-    if (correct === $button[3].textContent) {
-        window.alert('正解!');
-    }else {
-        window.alert('不正解!');
-    }
-});
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+    $button[handlerIndex].addEventListener('click', (e) => {
+        clickHndler(e);
+    });
+    handlerIndex++;
+};
+
