2aa5894883168207dce076b4f52edef8709e7fce
shunpii <shun.s0029@gmail.com>
2022-02-01 18:17:18 +0900

    [add]コメントの追加

--- javascript/index.js
+++ javascript/index.js
@@ -7,10 +7,11 @@ const answers = [
 ];
 const correct = 'ニンテンドーDS';
 
-console.log();
-
+// 定数の文字列をHTMLに反映させる
 document.getElementById('js-question').textContent = question;
 document.getElementsByTagName('button')[0].textContent = answers[0];
 document.getElementsByTagName('button')[1].textContent = answers[1];
 document.getElementsByTagName('button')[2].textContent = answers[2];
 document.getElementsByTagName('button')[3].textContent = answers[3];
+
+// ボタンをクリックしたら正誤判定
