4611571e817da853ca009bf7d7f5ca249b67fb59
shunpii <shun.s0029@gmail.com>
2022-02-01 18:11:25 +0900

    [add]選択肢をhtmlに反映

--- javascript/index.js
+++ javascript/index.js
@@ -10,3 +10,7 @@ const correct = 'ニンテンドーDS';
 console.log();
 
 document.getElementById('js-question').textContent = question;
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
