4dd8ffb7d809afcf68fbce1eb249191c39fe1182
shunpii <shun.s0029@gmail.com>
2022-02-02 16:02:36 +0900

    [update]次の問題に行くようにif文を追加

--- javascript/index.js
+++ javascript/index.js
@@ -31,28 +31,37 @@ const quiz = [
 const quizLength = quiz.length;
 let quizIndex = 0;
 
-
-
 const $button = document.getElementsByTagName('button')
 const buttonLength = $button.length;
 
 // クイズの問題文、選択肢を定義
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent = question;
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
     let buttonIndex = 0;
     while(buttonIndex < buttonLength){
         // ここに命令を書く
-        $button[buttonIndex].textContent = answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     }
 }
 setupQuiz();
 
+
 const clickHndler = (e) => {
-    if (correct === e.target.textContent){
+    if (quiz[quizIndex].correct === e.target.textContent){
         window.alert('正解!');
     }else {
         window.alert('不正解!');
+    }
+
+    quizIndex++;
+
+    if(quizIndex < quizLength){
+        // 問題数がまだあればこちらを実行
+        setupQuiz();
+    } else {
+        // 問題数がもうなければこちらを実行
+        window.alert('終了!');
     }
 };
 
